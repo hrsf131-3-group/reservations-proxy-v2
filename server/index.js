8e66c15f81b36c11b194c5193c1c3f310f4f9027
Linh Dang <linh.dang01@gmail.com>
2020-11-21 14:10:15 -0800

    configured proxy to connect to all rest-n-react services

--- server/index.js
+++ server/index.js
@@ -7,5 +7,6 @@ const server = require('./server.js');
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on localhost:${PORT}`);
 });
